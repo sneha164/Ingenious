5f09d58a46f1ef7e00ac78184f7a3e04530468ba
sneha <sneha.sapkota123@gmail.com>
2024-07-31 12:35:33 +0545

    all comonents made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {UserProfileComponent} from "./user-profile/user-profile.component";
+import {TeacherDetailsComponent} from "./teacher-details/teacher-details.component";
+import {AddTeacherComponent} from "./add-teacher/add-teacher.component";
+import {AddUserComponent} from "./add-user/add-user.component";
+import {UserCrudComponent} from "./user-crud/user-crud.component";
+import {ModuleDetailsComponent} from "./module-details/module-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,UserProfileComponent, TeacherDetailsComponent, AddTeacherComponent, AddUserComponent, TeacherDetailsComponent, UserCrudComponent,
+  ModuleDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AddUserComponent} from "./add-user/add-user.component";
+import {AddTeacherComponent} from "./add-teacher/add-teacher.component";
+import {ConfirmPasswordComponent} from "./confirm-password/confirm-password.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ErrorComponent} from "./error/error.component";
+import {ForgetPasswordComponent} from "./forget-password/forget-password.component";
+import {ModuleDetailsComponent} from "./module-details/module-details.component";
+import {OtpComponent} from "./otp/otp.component";
+import {PopupMessageComponent} from "./popup-message/popup-message.component";
+import {TeacherDetailsComponent} from "./teacher-details/teacher-details.component";
+import {UserCrudComponent} from "./user-crud/user-crud.component";
+import {UserProfileComponent} from "./user-profile/user-profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:"login", component:LoginComponent, title:"Login"},
+  { path:"add-teacher", component:AddTeacherComponent, title:"Add User"},
+  { path:"add-teacher", component:AddUserComponent, title:"Add User"},
+  { path:"confirm-password", component:ConfirmPasswordComponent, title:"Confirm Password"},
+  { path:"dashboard", component:DashboardComponent, title:"Dashboard"},
+  { path:"error", component:ErrorComponent, title:"Error"},
+  { path:"forget-password", component:ForgetPasswordComponent, title:"Forget Password"},
+  { path:"module-details", component:ModuleDetailsComponent, title:"Module Details"},
+  { path:"otp", component:OtpComponent, title:"OTP"},
+  { path:"popup-message", component:PopupMessageComponent, title:"Popup message"},
+  { path:"teacher-details", component:TeacherDetailsComponent, title:"Teacher Details"},
+  { path:"user-crud", component:UserCrudComponent, title:"User CRUD"},
+  { path:"user-profile", component:UserProfileComponent, title:"User Profile"},
+];
